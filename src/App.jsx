d2ad98c42c61a51c3806d03e517a7e918e7ffb5f
Lewis Lovett <llovett7575@gmail.com>
2023-09-01 13:04:19 +0100

    Created react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const 
+
+  return (
+    <body>
+
+    </body>
+      
+  );
+}
+
+export default App;
---
+++ src/component/SocialButton/SocialButton.jsx
@@ -0,0 +1,8 @@
+import "./SocialButton.scss";
+
+
+export default function SocialButton(){
+    return(
+        <button><i class="fa fa-twitter"></i></button>
+    )
+}
