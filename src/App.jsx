33960769ef7b04ab38775087628d3331b603c000
Lewis Lovett <llovett7575@gmail.com>
2023-09-04 14:36:42 +0100

    Added styles to match project image

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   
   const [primaryColor, setPrimaryColor] =useState('#16a085')
-  const [quote, setQuote] =useState('hello')
-  const [quoteAuthor, setQutoeAuthor] =useState('tim')
+  const [quote, setQuote] =useState('The battles that count aren’t the ones for gold medals. The struggles within yourself–the invisible battles inside all of us–that’s where it’s at.')
+  const [quoteAuthor, setQuoteAuthor] =useState('Jesse Owens')
   document.body.style.backgroundColor = primaryColor;
 
   useEffect(() =>{
@@ -21,10 +21,10 @@ function App() {
 
   return (
     <div className="wrapper">
-      <QuoteBlock quote={quote} quoteAuthor ={quoteAuthor} />
+      <QuoteBlock quote={quote} quoteAuthor ={quoteAuthor} primaryColor={primaryColor} />
       <SocialButton backgroundColor={primaryColor} socialIconName={'tumblr'} altText='Post this quote on tumblr!' socialUrl='https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Alice%20Walker&content=The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'twitter'} altText='Tweet this quote!' socialUrl='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.%22%20Alice%20Walker'/>
-      <button className='quoteBt' >New quote</button>
+      <button style={{backgroundColor:primaryColor}} className='wrapper__quoteButton' >New quote</button>
     </div>
       
   );
--- src/component/QuoteBlock/QuoteBlock.jsx
+++ src/component/QuoteBlock/QuoteBlock.jsx
@@ -1,11 +1,11 @@
 import './QuoteBlock.scss'
 
-export default function QuoteBlock({quote,quoteAuthor}){
+export default function QuoteBlock({quote,quoteAuthor,primaryColor}){
 
     return(
-        <div className="quoteBlock">
-            <div className="quoteBlock__quote">"{quote}</div>
-            <div className="quoteBlock__author">{quoteAuthor}</div>
+        <div  className="quoteBlock">
+            <div  className="quoteBlock__quote" style={{color:primaryColor}}><span className="quoteBlock__speechMark">"</span>{quote}</div>
+            <div  className="quoteBlock__author" style={{color:primaryColor}}>-{quoteAuthor}</div>
         </div>
     )
 }
