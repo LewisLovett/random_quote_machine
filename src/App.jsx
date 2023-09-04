3725c5c0b470416a715a45f490fc0f5f6ee7efa5
Lewis Lovett <llovett7575@gmail.com>
2023-09-04 16:13:25 +0100

    Random quote now generated from api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.scss';
 
 import SocialButton from './component/SocialButton/SocialButton';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import QuoteBlock from './component/QuoteBlock/QuoteBlock';
 
 
@@ -10,8 +10,7 @@ function App() {
 
   
   const [primaryColor, setPrimaryColor] =useState('#16a085')
-  const [quote, setQuote] =useState('The battles that count aren’t the ones for gold medals. The struggles within yourself–the invisible battles inside all of us–that’s where it’s at.')
-  const [quoteAuthor, setQuoteAuthor] =useState('Jesse Owens')
+  const [quote, setQuote] =useState({quoteText:'',author:''})
   document.body.style.backgroundColor = primaryColor;
 
   useEffect(() =>{
@@ -19,22 +18,28 @@ function App() {
   },[primaryColor])
 
   useEffect(() => {
-    const requestOptions ={
-      method:'GET',
-      headers: {'X-Api-Key':process.env.QUOTE_API_KEY}
-    }
-    fetch('https://api.api-ninjas.com/v1/quotes',requestOptions).then((response) => response.json()).then((data)=>{
-      console.log(data);
-    }).catch((err) =>{
-      console.log(err.message);
-    })
-  })
+     
 
+      const requestOptions ={
+        method:'GET',
+        headers: {'X-Api-Key':process.env.REACT_APP_QUOTE_API_KEY}
+      }
+      fetch('https://api.api-ninjas.com/v1/quotes',requestOptions).then((response) => response.json()).then((data)=>{
+        
+        setQuote({quoteText:data[0].quote,author:data[0].author});
+        
+      }).catch((err) =>{
+        console.log(err.message);
+      })
+ 
+  },[])
+
+ 
 
 
   return (
     <div className="wrapper">
-      <QuoteBlock quote={quote} quoteAuthor ={quoteAuthor} primaryColor={primaryColor} />
+      <QuoteBlock quote={quote} primaryColor={primaryColor} />
       <SocialButton backgroundColor={primaryColor} socialIconName={'tumblr'} altText='Post this quote on tumblr!' socialUrl='https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Alice%20Walker&content=The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'twitter'} altText='Tweet this quote!' socialUrl='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.%22%20Alice%20Walker'/>
       <button style={{backgroundColor:primaryColor}} className='wrapper__quoteButton' >New quote</button>
--- src/component/QuoteBlock/QuoteBlock.jsx
+++ src/component/QuoteBlock/QuoteBlock.jsx
@@ -1,11 +1,11 @@
 import './QuoteBlock.scss'
 
-export default function QuoteBlock({quote,quoteAuthor,primaryColor}){
+export default function QuoteBlock({quote,primaryColor}){
 
     return(
         <div  className="quoteBlock">
-            <div  className="quoteBlock__quote" style={{color:primaryColor}}><span className="quoteBlock__speechMark">"</span>{quote}</div>
-            <div  className="quoteBlock__author" style={{color:primaryColor}}>-{quoteAuthor}</div>
+            <div  className="quoteBlock__quote" style={{color:primaryColor}}><span className="quoteBlock__speechMark">"</span>{quote.quoteText}</div>
+            <div  className="quoteBlock__author" style={{color:primaryColor}}>-{quote.author}</div>
         </div>
     )
 }
