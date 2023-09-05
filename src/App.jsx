84d43054989b68cde58a86acb531ca6bca87e71f
Lewis Lovett <llovett7575@gmail.com>
2023-09-05 12:47:23 +0100

    Qutote button generates new quote and changes primary color

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,39 @@ function App() {
   },[primaryColor])
 
   useEffect(() => {
-     
+     getNewQuote();
 
-      const requestOptions ={
-        method:'GET',
-        headers: {'X-Api-Key':process.env.REACT_APP_QUOTE_API_KEY}
-      }
-      fetch('https://api.api-ninjas.com/v1/quotes',requestOptions).then((response) => response.json()).then((data)=>{
-        
-        setQuote({quoteText:data[0].quote,author:data[0].author});
-        
-      }).catch((err) =>{
-        console.log(err.message);
-      })
+      
  
   },[])
 
- 
+
+
+ const getNewQuote = () => {
+  const requestOptions ={
+    method:'GET',
+    headers: {'X-Api-Key':process.env.REACT_APP_QUOTE_API_KEY}
+  }
+  fetch('https://api.api-ninjas.com/v1/quotes',requestOptions).then((response) => response.json()).then((data)=>{
+    
+    setQuote({quoteText:data[0].quote,author:data[0].author});
+    
+  }).catch((err) =>{
+    console.log(err.message);
+  })
+
+ }
+
+ const genrateNewColor = () => {
+  var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  console.log(randomColor);
+  setPrimaryColor(randomColor);
+ }
+
+ const quoteButtonHandler = () => {
+  getNewQuote();
+  genrateNewColor();
+ }
 
 
   return (
@@ -42,7 +58,7 @@ function App() {
       <QuoteBlock quote={quote} primaryColor={primaryColor} />
       <SocialButton backgroundColor={primaryColor} socialIconName={'tumblr'} altText='Post this quote on tumblr!' socialUrl='https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Alice%20Walker&content=The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'twitter'} altText='Tweet this quote!' socialUrl='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.%22%20Alice%20Walker'/>
-      <button style={{backgroundColor:primaryColor}} className='wrapper__quoteButton' >New quote</button>
+      <button onClick={quoteButtonHandler} style={{backgroundColor:primaryColor}} className='wrapper__quoteButton' >New quote</button>
     </div>
       
   );
