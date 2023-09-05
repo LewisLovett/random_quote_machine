31a69d346c2d42ceb97c741e3be8dc4616e92521
Lewis Lovett <llovett7575@gmail.com>
2023-09-05 14:55:47 +0100

    Added github pages package

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 
 
   return (
-    <div className="wrapper">
+    <div id="quote-box" className="wrapper">
       <QuoteBlock quoteOpacity={quoteOpacity} quote={quote} primaryColor={primaryColor}/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'tumblr'} altText='Post this quote on tumblr!' socialUrl='https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Alice%20Walker&content=The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'twitter'} altText='Tweet this quote!' socialUrl='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.%22%20Alice%20Walker'/>
