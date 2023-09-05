4d5d27a7d51f2a69b5722f0da0dd735e913ec7c5
Lewis Lovett <llovett7575@gmail.com>
2023-09-05 14:43:30 +0100

    Added transitions

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   
   const [primaryColor, setPrimaryColor] =useState('#16a085')
   const [quote, setQuote] =useState({quoteText:'',author:''})
+  const [quoteOpacity, setQuoteOpacity] =useState(1)
   document.body.style.backgroundColor = primaryColor;
 
   useEffect(() =>{
@@ -27,6 +28,7 @@ function App() {
 
 
  const getNewQuote = () => {
+  
   const requestOptions ={
     method:'GET',
     headers: {'X-Api-Key':process.env.REACT_APP_QUOTE_API_KEY}
@@ -34,7 +36,7 @@ function App() {
   fetch('https://api.api-ninjas.com/v1/quotes',requestOptions).then((response) => response.json()).then((data)=>{
     
     setQuote({quoteText:data[0].quote,author:data[0].author});
-    
+    setTimeout(setQuoteOpacity(1),500);
   }).catch((err) =>{
     console.log(err.message);
   })
@@ -43,19 +45,20 @@ function App() {
 
  const genrateNewColor = () => {
   var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  console.log(randomColor);
   setPrimaryColor(randomColor);
  }
 
  const quoteButtonHandler = () => {
-  getNewQuote();
-  genrateNewColor();
+  setQuoteOpacity(0);
+  setTimeout(getNewQuote,500);
+  setTimeout(genrateNewColor,500);
+  
  }
 
 
   return (
     <div className="wrapper">
-      <QuoteBlock quote={quote} primaryColor={primaryColor} />
+      <QuoteBlock quoteOpacity={quoteOpacity} quote={quote} primaryColor={primaryColor}/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'tumblr'} altText='Post this quote on tumblr!' socialUrl='https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Alice%20Walker&content=The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'/>
       <SocialButton backgroundColor={primaryColor} socialIconName={'twitter'} altText='Tweet this quote!' socialUrl='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20most%20common%20way%20people%20give%20up%20their%20power%20is%20by%20thinking%20they%20don%E2%80%99t%20have%20any.%22%20Alice%20Walker'/>
       <button onClick={quoteButtonHandler} style={{backgroundColor:primaryColor}} className='wrapper__quoteButton' >New quote</button>
--- src/component/QuoteBlock/QuoteBlock.jsx
+++ src/component/QuoteBlock/QuoteBlock.jsx
@@ -1,10 +1,10 @@
 import './QuoteBlock.scss'
 
-export default function QuoteBlock({quote,primaryColor}){
+export default function QuoteBlock({quoteOpacity, quote, primaryColor}){
 
     return(
-        <div  className="quoteBlock">
-            <div  className="quoteBlock__quote" style={{color:primaryColor}}><span className="quoteBlock__speechMark">"</span>{quote.quoteText}</div>
+        <div style={{opacity:quoteOpacity}}  className="quoteBlock">
+            <div  className="quoteBlock__quote" style={{color:primaryColor}}><span className="quoteBlock__speechMark">"</span><span className="quoteBlock__quoteText">{quote.quoteText}</span></div>
             <div  className="quoteBlock__author" style={{color:primaryColor}}>-{quote.author}</div>
         </div>
     )
